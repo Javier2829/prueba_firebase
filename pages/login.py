c01c35037d2088e5787acb8ae10cc56200be4d17
Javier <wild2829@gmail.com>
2025-07-12 22:54:39 -0400

    login

--- pages/login.py
+++ pages/login.py
@@ -5,20 +5,28 @@ def generarMenu(param):
     pass
 
 
+def validarUsuario(user, pasw):
+    if user == 'fredy' and pasw == '1526':
+        return
+    else:
+        st.toast('Debes completar los dato', icon=":material/error:")
+
+
 def generarLogin():
     if 'usuario' in st.session_state:
         generarMenu(st.session_state['usuario'])
     else:
-        with st.form('frmLogin', clear_on_submit= True):
-            user= st.text_input('User')
-            pasw= st.text_input('User', type='password')
-            btLogin= st.form_submit_button('Ingresar')
+        with st.form('frmLogin', clear_on_submit=True):
+            user = st.text_input('User')
+            pasw = st.text_input('User', type='password')
+            btLogin = st.form_submit_button('Ingresar')
 
             if btLogin:
-                if user=='fredy' and pasw=='1526':
+                if validarUsuario(user, pasw):
+                    st.session_state['usuario'] = user
                     st.info('logueado')
                 else:
-                    st.error('usuario o clave invalido', icon=":material/error:")
+                    st.toast('Ingresar una contraseña')
 
 
 generarLogin()
